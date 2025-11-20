eea1d1be5081a19e022d7e68e043985616a9ed22
yann <yannhenaff63@gmail.com>
2025-11-20 10:34:17 +0100

    Résolution des conflits

--- include/liste.h
+++ include/liste.h
@@ -12,4 +12,5 @@ node_t *list_create(void);
 node_t *list_insert(node_t *head,void *data);
 node_t *list_next(node_t *head);
 void *list_get_data(const node_t * node);
+node_t * list_headRemove(node_t * head);
 #endif
--- src/liste.c
+++ src/liste.c
@@ -32,3 +32,13 @@ node_t *list_insert(node_t *head,void *data){
     head = n1;
     return head;
 };
+
+node_t * list_headRemove(node_t * head){
+    if(head==NULL)return NULL;
+    //surpimme la tete de list
+    node_t *temp=head;
+    head = head->next;
+    free(temp);
+    //retourne la nouvelle tete
+    return head;
+}
